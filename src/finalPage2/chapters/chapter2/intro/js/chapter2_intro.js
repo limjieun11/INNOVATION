b4ebab216483c3c34323a83f58f01b8588532d3f
“limjieun11” <1129127080+limjieun11@users.noreply.github.com>
2025-09-22 03:48:33 +0900

    0922

--- src/finalPage2/chapters/chapter2/intro/js/chapter2_intro.js
+++ src/finalPage2/chapters/chapter2/intro/js/chapter2_intro.js
@@ -1,10 +1,10 @@
 // chapters/chapter2/intro/js/chapter2_intro.js
 
-document.addEventListener('DOMContentLoaded', () => {
-  const startButton = document.getElementById('start-button');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const startButton = document.getElementById('start-button');
 
-  startButton.addEventListener('click', () => {
-    // myroom.html로 이동 (루프 시작점)
-    window.location.href = '../scenes/myroom.html';
-  });
-});
+//   startButton.addEventListener('click', () => {
+//     // myroom.html로 이동 (루프 시작점)
+//     window.location.href = '../scenes/myroom.html';
+//   });
+// });
